83333955ad7baf47280579c01d4ad472937ef1dc
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-29 09:31:00 +0400

    .

--- src/pgdp/threads/ParallelMergeSort.java
+++ src/pgdp/threads/ParallelMergeSort.java
@@ -1,14 +1,36 @@
 package pgdp.threads;
 
+import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveAction;
 
 public class ParallelMergeSort extends RecursiveAction {
+
+    private Comparable[] array;
+    private int low, mid, high;
+
     public ParallelMergeSort(Comparable[] array) {
-        // TODO
+        Comparable[] helper = new Comparable[array.length];
+        ParallelMergeSort parallelMergeSort = new ParallelMergeSort(array, helper, 0, array.length-1);
+        parallelMergeSort.compute();
+    }
+
+    public ParallelMergeSort(Comparable[] array, Comparable[] helper, int leftIndex, int rightIndex) {
+        this.array = array;
+        low = leftIndex;
+        high = rightIndex;
+        mid = (low+high)/2;
     }
 
     @Override
     protected void compute() {
-        // TODO
+        if (low<high) {
+
+            ParallelMergeSort left = new ParallelMergeSort(array, new Comparable[mid], low, mid);
+            ParallelMergeSort right = new ParallelMergeSort(array, new Comparable[array.length-mid], mid+1, high);
+
+            invokeAll(left, right);
+
+            MergeSort.merge(array, new Comparable[array.length], low, mid, high);
+        }
     }
 }
